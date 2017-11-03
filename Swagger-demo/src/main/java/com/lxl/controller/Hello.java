4760beb79d6db63d3eadba61a34058048ba51435
lxlWORK <460668853@qq.com>
2017-11-03 17:48:09 +0800

    解决跨域问题

--- Swagger-demo/src/main/java/com/lxl/controller/Hello.java
+++ Swagger-demo/src/main/java/com/lxl/controller/Hello.java
@@ -14,11 +14,12 @@ import com.wordnik.swagger.annotations.ApiOperation;
 
 	@Controller
 	@RequestMapping(value="/lxl")
+	@Api(value = "lxl")
 	public class Hello {  
 	@Autowired
 	private NameService nameservice;
-    @RequestMapping(value="/name",method=RequestMethod.GET)
-    @ApiOperation(value="获取人名7")
+    @RequestMapping(value="/name/{name}",method=RequestMethod.GET)
+    @ApiOperation(value="根据姓名获取用户")
     public String HelloWorld(Model model){  
         model.addAttribute("message",nameservice.getname());  
         return "HelloWorld";  
